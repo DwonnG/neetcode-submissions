c981ec856a8657bdf2abc86c4460dd25d2dc87b4
Dwonn D. Goodwin <DwonnGoodwin@gmail.com>
2026-05-18 23:33:40 -0400

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
+        openings = bracket_map.values()
+        for char in s:
+            if char in openings:
+                stack.append(char)
+            elif not stack or bracket_map[char] != stack[-1]:
+                return False
+            else:
+                stack.pop()
+        
+        return not stack
+
