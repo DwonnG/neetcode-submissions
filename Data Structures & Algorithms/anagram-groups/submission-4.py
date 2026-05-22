3621bb2336d686457a8fb45f88de4a0aaed4fcc0
Dwonn D. Goodwin <DwonnGoodwin@gmail.com>
2026-05-22 12:53:11 -0400

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        answer = defaultdict(list)
+        for s in strs:
+            anagram = "".join(sorted(s))
+            answer[anagram].append(s)
+        
+        return list(answer.values())
